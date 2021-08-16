f68885209ea5ead98ebbb71e7a4b9ad52f9fd592
AdvancingHermit <christiannkya@outlook.com>
2021-08-16 11:07:56 +0200

    First Assignmetn

---
+++ Svar01.java
@@ -0,0 +1,8 @@
+class Svar01{
+	public static void main(String[] args) {
+		System.out.println(3.3 == 1.1 + 1.1 + 1.1);
+		System.out.println(0.3 == 0.1 + 0.1 + 0.1);
+		System.out.println("Hello, World!");
+
+	}
+}
---
+++ sketch_210816a.pde
@@ -0,0 +1,5 @@
+void start(){
+  println("Hello World");
+  println("æøå ÆØÅ");
+  println(3.3 == 1.1+1.1+1.1);
+}
